622bdd7f25e4459ad17d275f347b7691dd905a1e
ItsukiKawamoto <s2112073@s.tsukuba.ac.jp>
2022-04-23 18:46:33 +0900

    分割数を変化させながら複数回計算

--- sp1_2.c
+++ sp1_2.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 int main() {
-    int bandNum = 1000;
-    double start = 0.0;
-    double end = 2.0;
-    double dx = (end - start) / bandNum;
-    double sum = 0.0;
-    for (double i = start; i < end; i += dx) {
-        sum += (pow(i, 3.0) + pow((i + dx), 3.0)) * dx / 2.0;
+    const double start = 0.0;
+    const double end = 2.0;
+    for (int bandNum = 1000; bandNum < 10000; bandNum += 100) {
+        double dx = (end - start) / bandNum;
+        double sum = 0.0;
+        for (double i = start; i < end; i += dx) {
+            sum += (pow(i, 3.0) + pow((i + dx), 3.0)) * dx / 2.0;
+        }
+        printf("%d : ", bandNum);
+        printf("%lf\n", sum);
     }
-    printf("%lf", sum);
 }
