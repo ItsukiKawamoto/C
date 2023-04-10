d0c70d67484c855e5e314f6fc7a5cc9327153b5b
ItsukiKawamoto <s2112073@s.tsukuba.ac.jp>
2023-04-10 18:48:43 +0900

    test

---
+++ sample.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int addup(int i); // 関数のプロトタイプ宣言
+
+int main() {
+    int i;
+    int sum;
+    i = 0;
+    sum = 0;
+    sum = addup(i);
+    // printf("i was squared - it now equals %d\n", sum);
+}
+
+int addup(int i) {
+    while (i <= 5) {
+        i = i + 1;
+        printf("i is %d\n", i * i); // i * iの結果を出力
+    }
+    i = i * i;
+    return (i);
+}
